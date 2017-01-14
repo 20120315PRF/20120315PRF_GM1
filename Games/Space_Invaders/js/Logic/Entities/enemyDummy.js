4cc4d8de2777b8d21cdf42a793d0ef3695212f75
BattleChain-Lisk <luismiguel.louzao@louinc.es>
2017-01-14 22:49:14 +0100

    Añadida lógica para que el juego funcione de forma continuada.
    Se añade entrada de las naves enemigas.

--- Games/Space_Invaders/js/Logic/Entities/enemyDummy.js
+++ Games/Space_Invaders/js/Logic/Entities/enemyDummy.js
@@ -13,26 +13,22 @@ enemyDummy.prototype.create = function(){
     this.enemiesDummy.enableBody = true;
     this.enemiesDummy.physicsBodyType = Phaser.Physics.ARCADE;
     
-    for(var y = 0; y<6;++y)    
-    {
-        for(var x = 0; x<7;++x)    
-        {
-            this.createEnemyDummy(x*80, y*30);
-        }
-    }
-    this.enemiesDummy.position = new Phaser.Point(this.game.width*0.2,this.game.height*0.08);
-    
+    this.enemiesDummy.position.x = this.game.width*0.2;
     //Animacion 
     //Se moverá horizontalmente 100 unidades, el tiempo 1 segundo, el efecto phaser.easing, 
-    var animacion = this.game.add.tween(this.enemiesDummy).to({x:100},1500,Phaser.Easing.Linear.None, true, 0, 1500, true);
+    this.animacion = this.game.add.tween(this.enemiesDummy).to({x:100},1500,Phaser.Easing.Linear.None, true, 0, 1500, true);
     //Le decimos a la animacion, que en cada loop descienda:
-    animacion.onRepeat.add(this.descender,this);
+    this.animacion.onRepeat.add(this.descender,this);
+    
+    this.initializeNewGroup(-0.15);
+    
+    
 
 }
 
 enemyDummy.prototype.descender=function()
 {
-    if(this.numDescending!=0)    
+    if(this.numDescending!=0 && this.enemiesDummy.position.y > 0.08)    
     {
         this.enemiesDummy.y += Configuracion.Game.velocityDescendingEnemies;
         --this.numDescending;
@@ -40,7 +36,7 @@ enemyDummy.prototype.descender=function()
     
 }
 enemyDummy.prototype.update = function(velocityShootEnemyDummy, bulletsObject){
-    if(this.game.time.now>this.aux_velocityShootEnemyDummy)    
+    if(this.game.time.now>this.aux_velocityShootEnemyDummy && this.enemiesDummy.position.y > 40)    
     {
         var shooter = this.findEnemyShooter();
         if (shooter)
@@ -48,6 +44,16 @@ enemyDummy.prototype.update = function(velocityShootEnemyDummy, bulletsObject){
             bulletsObject.fireBullet(shooter.body.x+16,shooter.body.y+16,shooter.rotation/2*-1);
             this.aux_velocityShootEnemyDummy = this.game.time.now + velocityShootEnemyDummy;
         } 
+    }
+    
+    if(this.enemiesDummy.position.y < 40.1)
+    {
+        this.enemiesDummy.y += 1.7;
+        
+        if(this.enemiesDummy.position.y>40)
+        {
+            this.animacion.resume();
+        }
     }
 
 }
@@ -70,7 +76,8 @@ enemyDummy.prototype.findEnemyShooter = function()
     else{
         
         //TODO
-        gameMgr.playerKill();
+        //gameMgr.playerKill();
+        this.initializeNewGroup(-0.45);    
     }
     
     return shooter;
@@ -86,4 +93,19 @@ enemyDummy.prototype.createEnemyDummy = function(posX, posY){
 enemyDummy.prototype.getEnemiesDummy = function()
 {
     return this.enemiesDummy;
+}
+
+enemyDummy.prototype.initializeNewGroup = function(height)
+{
+    for(var y = 0; y<6;++y)    
+        {
+            for(var x = 0; x<7;++x)    
+            {
+                this.createEnemyDummy(x*80, y*30);
+            }
+        }
+        
+    this.enemiesDummy.position.y = this.game.height*height;
+    this.numDescending = 30;
+    this.animacion.pause();
 }
