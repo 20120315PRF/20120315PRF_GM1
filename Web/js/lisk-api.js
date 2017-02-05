449844cc2b4c49d002b0cbcb8da6136a113ee98e
BattleChain-Lisk <luismiguel.louzao@louinc.es>
2017-02-05 16:58:14 +0100

    Add new thumb images for games and set a modal to change account settings

--- Web/js/lisk-api.js
+++ Web/js/lisk-api.js
@@ -1,24 +1,91 @@
-var LISK_EXPLORER_API_URL = "https://explorer.lisk.io/api/";
+var LISK_EXPLORER_API_URL = "https://testnet-explorer.lisk.io/api/";
+var HARDCODED_REDHAWK_ADDRESS = "937865154584638568L";
 
-var HARDCODED_REDHAWK_ADDRESS = "106014395311695277L";
-var HARDCODED_COMMON_FUND_ADDRESS = "15841793714384967784L";
- 
+//localstorage key
+var LISK_CONFIG_ADDRESS = "lisk_address";
 
-function renderRestData(data){
+var currentAddress = HARDCODED_REDHAWK_ADDRESS;
 
-    if(data['delegate']){
-        $('#delegate-name').text(data['delegate']['username']);
+// Address pattern for all addresses
+var addressPattern = /[0-9]{18}L/gi;
+
+
+
+/** Local Storage enabled to store configuration of direction */
+function localStorageEnabled(){
+    return typeof(Storage) !== "undefined";
+}
+
+
+// Load address from local storage
+function loadAddress(){
+    if(localStorageEnabled()){
+        var localStr = localStorage.getItem(LISK_CONFIG_ADDRESS);
+        if(localStr == null) return "";
+        else return localStr;
+    }
+}
+
+
+//Save address on local storage
+function saveAddress(newAddress){
+    if(localStorageEnabled()){
+        localStorage.setItem(LISK_CONFIG_ADDRESS, newAddress);
+    }
+}
+
+
+// Test if an address is valid
+function addressIsValid(candidateAddress){
+    return candidateAddress
+        && addressPattern.test(candidateAddress);
+}
+
+
+// Re-load data for the given address
+function reloadChanges(){
+    var candidateAddress = $('#account-address').val();
+    
+    if(addressIsValid(candidateAddress)){
+        
+        $('.input-error').removeClass('input-error');
+        
+        currentAddress = candidateAddress;
+        $('#welcome-message').hide();
+        $('#config-modal').modal('toggle');
+
+        //Update data
+        saveAddress(currentAddress);
+        getLiskAmmount(currentAddress);
+        
     } else {
-        $('#delegate-name').text(data['address']);
+            // SHOW ERROR
+            $('#account-address').addClass('input-error');
+        
     }
     
-    $('#lsk-ammount').text(parseInt(data['balance'])/100000000);
     
-    // Show data
+}
+
+
+// Show data on the screen
+function renderRestData(data){
+    if(data){
+        if(data['delegate']){
+            $('#delegate-name').text(data['delegate']['username']);
+        } else {
+            $('#delegate-name').text(data['address']);
+        }
+
+        $('#lsk-ammount').text(parseInt(data['balance'])/100000000);
+    }
+    
     $('#welcome-message').fadeIn();
 
 }
 
+
+// Rest call for getting account
 function getLiskAmmount(addressID){ 
     $.ajax({
         url: LISK_EXPLORER_API_URL + "getAccount?address=" + addressID,
@@ -35,7 +102,8 @@ function getLiskAmmount(addressID){
 
 
 $().ready(function(){
-
-    getLiskAmmount(HARDCODED_COMMON_FUND_ADDRESS);
-
+    if(localStorageEnabled){
+        currentAddress = loadAddress();
+    }
+    getLiskAmmount(currentAddress);
 });
